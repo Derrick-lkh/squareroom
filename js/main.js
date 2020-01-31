493afe283cad0262c4f5e9cdaee483f878c40b72
derrickSP <57939887+derrickSP@users.noreply.github.com>
2020-01-31 17:50:27 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  console.log("ready");
+
+  $(".animate-link").hover(
+    function() {
+      $(this)
+        .find(".underline")
+        .animate(
+          {
+            width: "100%"
+          },
+          500
+        );
+    },
+    function() {
+      $(this)
+        .find(".underline")
+        .animate(
+          {
+            width: "40%"
+          },
+          500
+        );
+    }
+  );
+});
+
+$(window).scroll(function(event) {
+  var scroll = $(window).scrollTop();
+  toggleNav(scroll);
+});
+
+function toggleNav(scroll) {
+  if (scroll > 1) {
+    $(".nav").css({
+      background: "white"
+    });
+    $(".logo-container img").css({
+      width: "90px",
+      height: "40px"
+    });
+    $(".nav-links li").css({
+      color: "black"
+    });
+  } else {
+    $(".nav").css({
+      background: "none"
+    });
+    $(".nav-links li").css({
+      color: "white"
+    });
+
+    $(".logo-container img").css({
+      width: "180px",
+      height: "80px"
+    });
+  }
+}
